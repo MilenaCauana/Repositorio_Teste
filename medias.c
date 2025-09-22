da2dfad584efec9b4c56116f1236d2c568867ead
Igor Marani <igormarani@outlook.com>
2025-09-22 16:40:49 -0300

    main verdadeira

--- medias.c
+++ medias.c
@@ -13,3 +13,66 @@ float mediaSimples(float nota1, float nota2)
     return (nota1 + nota2) / 2;
 }
 
+int main (){
+
+    int num_alunos;
+    float pesoP1, pesoP2;
+
+    printf("\nDigite o número de Alunos: ");
+    scanf("%d", &num_alunos);
+
+    printf("\nDigite o peso da p1:");
+    scanf("\n%2f", &pesoP1);
+
+    printf("\nDigite o peso da p2:");
+    scanf("\n%2f", &pesoP2);
+
+    if(pesoP1 == pesoP2){
+
+        printf("\n------------------------------------");
+
+        for(int i = 0; i < num_alunos; i++){
+
+            float p1, p2;
+
+            printf("\n\nAluno %d", i+1);
+            printf("\nDigite a nota da p1:");
+            scanf("%2f", &p1);
+            printf("\nDigite a nota da p2:");
+            scanf("%2f", &p2);
+
+            float mediaS = mediaSimples(p1, p2);
+
+            mensagemAprovacao(mediaS);
+
+            printf("\n....................");
+
+        }
+
+    }else{
+
+        printf("\n------------------------------------");
+
+        for(int i = 0; i < num_alunos; i++){
+
+            float p1, p2;
+
+            printf("\n\nAluno %d", i+1);
+            printf("\nDigite a nota da p1:");
+            scanf("%2f", &p1);
+            printf("\nDigite a nota da p2:");
+            scanf("%2f", &p2);
+
+            float mediaP = mediaPonderada(pesoP1, p1, pesoP2, p2);
+
+            mensagemAprovacao(mediaP);
+
+            printf("\n....................");
+
+        }
+
+    }
+
+    printf("\n------------------------------------");
+    return 0;
+}
