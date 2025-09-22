5509bc506668822888c716a9d8216050adc6d25e
Caionest <caionest06@gmail.com>
2025-09-22 16:14:07 -0300

    .

--- medias.c
+++ medias.c
@@ -1,3 +1,5 @@
+#include <stdbool.h>
+
 //Teste de Branch e Merge - Milena e Caio
 
 //parte do Caio
@@ -13,3 +15,13 @@ float mediaSimples(float nota1, float nota2)
     return (nota1 + nota2) / 2;
 }
 
+//função que verifica se foi aprovado
+
+bool aprovado(float media){
+    if(media >= 5){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
